f2d2ae0b29318f84bca4bddf144e6b0be73a7f51
Juan Sandoval <juansandovalvicente@gmail.com>
2025-01-02 20:33:47 -0500

    02 Nuevos archivos en carpeta Notebooks

---
+++ notebooks/01_OperacionesAritmeticas.py
@@ -0,0 +1,15 @@
+def sumar(a, b):
+    return a + b
+
+def restar(a, b):
+    return a - b
+
+if __name__ == "__main__":  
+    num1 = float(input("Introduce el primer número: "))  
+    num2 = float(input("Introduce el segundo número: "))  
+
+    suma = sumar(num1, num2)  
+    resta = restar(num1, num2)  
+
+    print(f"La suma de {num1} y {num2} es: {suma}")  
+    print(f"La resta de {num1} y {num2} es: {resta}") 
